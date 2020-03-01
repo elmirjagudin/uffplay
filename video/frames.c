34965a8fadd404245519e2ae9b3cdac9ba5405a5
elmir <elmir@brab>
2020-03-01 13:38:49 +0100

    convert to RGB24 format

--- video/frames.c
+++ video/frames.c
@@ -1,28 +1,26 @@
 #include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+
+#define TEXTURE_PIX_FMT AV_PIX_FMT_RGB24
 
 #define MOV_FILE "foo.mov"
 
-static void 
-pgm_save(unsigned char *buf, int wrap, int xsize, int ysize, char *filename)
+static void
+rgb24_save(uint8_t *pixels, int len)
 {
     FILE *f;
-    int i;
 
-    f = fopen(filename,"w");
-    fprintf(f, "P5\n%d %d\n%d\n", xsize, ysize, 255);
-    for (i = 0; i < ysize; i++)
-    {
-        fwrite(buf + i * wrap, 1, xsize, f);
-    }
+    f = fopen("foo", "w");
+    fwrite(pixels, len, 1, f);
     fclose(f);
+
 }
 
-
-int 
+int
 main(int argc, char **argv)
 {
     int ret;
-    AVFormatContext *fmt_ctx = NULL; //avformat_alloc_context();
+    AVFormatContext *fmt_ctx = NULL;
 
     ret = avformat_open_input(&fmt_ctx, MOV_FILE, NULL, NULL);
     if (ret < 0)
@@ -38,7 +36,7 @@ main(int argc, char **argv)
     }
 
     ret = av_find_best_stream(fmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0);
-    if (ret < 0) 
+    if (ret < 0)
     {
         fprintf(stderr, "Could not find %s stream in input file '%s'\n",
                 av_get_media_type_string(AVMEDIA_TYPE_VIDEO), MOV_FILE);
@@ -85,9 +83,26 @@ main(int argc, char **argv)
         return 1;
     }
 
+    struct SwsContext *sws_ctx = sws_getContext(dec_ctx->width,
+                                                dec_ctx->height,
+                                                dec_ctx->pix_fmt,
+                                                dec_ctx->width,       // do 1:1 conversion
+                                                dec_ctx->height,
+                                                TEXTURE_PIX_FMT,
+                                                0, NULL, NULL, NULL);
+    if (sws_ctx == NULL) {
+        fprintf(stderr, "error creating SwsContext\n");
+        return 1;
+    }
+
+    uint8_t *pixels[1];
+    int linesize[1];
+
+    pixels[0] = malloc(dec_ctx->width * dec_ctx->height * 3);
+    linesize[0] = dec_ctx->width * 3;
 
     AVFrame *frame = av_frame_alloc();
-    if (!frame) 
+    if (!frame)
     {
         fprintf(stderr, "Could not allocate frame\n");
         return 1;
@@ -96,7 +111,7 @@ main(int argc, char **argv)
     int i;
     int got_frame;
 
-    for (i = 0; i < 32*10 ; i++)
+    for (i = 0; i < 32 ; i++)
     {
         AVPacket pkt = { 0 };
 
@@ -106,7 +121,7 @@ main(int argc, char **argv)
         {
             printf("not a video pkt, skipping");
             av_packet_unref(&pkt);
-            continue;    
+            continue;
         }
 
         ret = avcodec_send_packet(dec_ctx, &pkt);
@@ -120,12 +135,16 @@ main(int argc, char **argv)
         if (ret == 0)
         {
             printf("got frame\n");
-            /* the picture is allocated by the decoder. no need to
-               free it */
-            char buf[1024];
-            snprintf(buf, sizeof(buf), "%s-%d", "filename", dec_ctx->frame_number);
-            pgm_save(frame->data[0], frame->linesize[0],
-                     frame->width, frame->height, buf);
+
+            sws_scale(sws_ctx,
+                      (const uint8_t *const*)frame->data,
+                      frame->linesize,
+                      0,
+                      frame->height,
+                      (uint8_t *const*)pixels,
+                      linesize);
+
+            rgb24_save(pixels[0], dec_ctx->width * dec_ctx->height * 3);
         }
         else if (ret == AVERROR(EAGAIN))
         {
