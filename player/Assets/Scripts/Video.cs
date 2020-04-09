32bd3a5ade3d9fb2daf64c696a68b772d45bd07d
elmir <elmir@brab>
2020-04-09 15:13:47 +0200

    WIP: model-view-controller

---
+++ player/Assets/Scripts/Video.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///
+/// Video model object in the model-viewer-controller triad
+///
+public class Video
+{
+    enum Playback
+    {
+        Playing,
+        Paused,
+        Closed,
+    }
+
+    public static void Open(string FileName)
+    {
+
+    }
+
+    public static void Play()
+    {
+
+    }
+
+    public static void Pause()
+    {
+
+    }
+
+    public static void Seek(long FramePTS)
+    {
+
+    }
+}
